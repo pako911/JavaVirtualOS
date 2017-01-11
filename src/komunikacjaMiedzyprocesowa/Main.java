5ebf454738a76b3926928dac4e863b9252281977
pedeef <przemyslaw.lapicz@gmail.com>
2017-01-11 13:39:47 +0100

    Update Main.java

--- src/komunikacjaMiedzyprocesowa/Main.java
+++ src/komunikacjaMiedzyprocesowa/Main.java
@@ -1,9 +1,10 @@
 package komunikacjaMiedzyprocesowa;
 
 import java.io.*;
-//PROTEZY - Proces.java i ListaProcesow.java
+//PROTEZY - Proces.java, ListaProcesow.java i Czytnik.java
 public class Main {
 	//WAZNE
+	//WYMAGANE UTWORZENIE 4 PROCESOW
 	//tworzy skrzynke gdy proces wysle wiadomosc - i staje sie jej wlascicielem
 	//jak pobierze komende zeby wyslac XS to IPC.wyslij(wiadomosc,PID);
 	//jak pobierze komende zeby odebrac XR to IPC.odbierz(PID);
@@ -18,18 +19,36 @@ public class Main {
 	//DODAC ZE BLOKUJE PROCES CZY COS
 	
 	//przykladowe wpisywanie:
-	//czy praca krokowa? (t/n) : t
 	//XC program1
+	//XC program2
+	//XC program3
+	//XC program4
 	//cd,cd(...) w miedzyczasie jak chce sprawdzic stan skrzynek to SAB lub SB nr_procesu
 	//gdy dostane informacje o zakonczonym procesie : EXIT
+	//ale po zakonczeniu wszystkich procesow tablica tych czytnikow - interpreterow do kazdego z plikow nie sa usuwane
+	//zrobione tylko do sprawdzenia i ogolnego pogladu
 	//tyle
 	
+	/*
+	 co do wykorzystania:
+	 sytuacja taka: sa utworzone dwa procesy, 1,2,3,4
+	 Przyklad 1:
+	 jesli proces 1 chce wyslac do 2 rozkaz XS 2 wiadomosc(IPC.wyslij(wiadomosc,PIDodbiorcy), 
+	 jak odebrac to XR(IPC.odbierz(proces_obecny)
+	 jesli bedzie chcial wyslac do innego, np 4, to przed wykonaniem XS czy proces istnieje <czy jego numer istnieje> forem czy cos
+	 dodatkowo, przy wyslaniu do procesu ktorego nie ma po tym ma sie zawiesic i zmienic numer procesu
+	 gdy proces chce czytac ze skrzynki a w niej nic nie bedzie lub skrzynka nie istnieje if(!IPC.odbierz(id))
+	 usuniecie skrzynki nastepuje razem z usunieciem procesu
+	 //procesy dzialaja po kolei: 1,2,3,4,1,2,3,4 do konca
+	 */
 	static int id=0;
-	public static void main(String[] args) throws InterruptedException, IOException {
+	static int i=0;
+	static int j=-1;
+	static Czytnik[] cz = new Czytnik[20];
+	public static void main(String[] args) throws IOException {
 		PrintWriter pw = new PrintWriter(System.out, true); 						//obiekty do wyswietlania
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 	//i wpisywania w konsoli
 		String str; 			//zmienne operujace na danych z konsoli
-		String s; 				//zmienna do czytania z pliku wejsciowego
 		//TEST
 		pw.println(" @@@@   @@   @     @   @@      @@@   @@@   ");
 		pw.println("    @  @  @   @   @   @  @    @     @@ @@  ");
@@ -41,100 +60,60 @@ public class Main {
 		pw.println("*******************************************");
 		pw.println("*POMOC: WPISZ HELP");
 		pw.println("->DZIALAJ!<-");
-		
+		pw.println("PAMIETAJ !");
+		pw.println("kontynuuj 'cd'");
 		boolean wyjscie = false; 	//wyjscie z testowego modulu
 		//CALE DZIALANIE MODULU
 		do
 		{			
 			String nazwaPliku;			
 			String sciezka;	
-			boolean dzialanie = false; 	//zmienna czy proces zostal zakonczony - komenda XD
-					pw.println("PAMIETAJ !");
-					pw.println("kontynuuj 'cd'");
-					str = br.readLine(); 			//wpisanie komendy
-					
-					if(str.startsWith("XC")){ 		//komenda do tworzenia procesu
-						pw.println("->KOMENDA XC");  	//tak o zebym wiedzial ze zadzialalo to akurat xd zbedne
-						id++;
-						//ListaProcesow.wyswietlWszystkieProcesy(); testowne odpowiedzi
-						//ListaProcesow.wyswietlPCBProcesu(id);
-						ListaProcesow.dodajProces(id);
-						dzialanie=true;
-						//ListaProcesow.wyswietlPCBProcesu(id);
-						//ListaProcesow.wyswietlWszystkieProcesy();
-						nazwaPliku = str.substring(3); 							//podzielenie wiersza z komenda aby wydobyc nazwe pliku wejsciowego
-						sciezka = "c:\\JavaSO\\"+nazwaPliku+".txt"; 	//utworzenie sciezki do pliku aby go otworzyc
-						FileReader fr = new FileReader(sciezka);			//otworzenie pliku do odczytu
-						BufferedReader brplik = new BufferedReader(fr);  //do czytania
-					do{
-						str = br.readLine(); //pobranie komendy
-					if(str.startsWith("cd")){
-						if((s = brplik.readLine()) != null) 			//pobieranie po wierszu
-						{		
-							ListaProcesow.iterujLinie(id);
-							pw.println("Wykonanie polecenia: "+s); 		//informacyjnie
-							if(s.startsWith("XR")){  					//test komendy, proces ma czytac komunikat
-								pw.println("->KOMENDA XR "); 			//rowniez informacyjnie, jak dalsze zaczynajace sie na "->"
-								int y = Integer.parseInt(s.substring(3,4)); //pobranie numeru procesu, z ktorego bedzie czytac ( probowal)
-								if(!IPC.odbierz(y))
-									System.out.println("proces "+id+" ma byc zablokowany");//wlasciwa metoda odpowiedzialna za czytanie, jesli odczyta wiadomosc 
-									//ListaProcesow.zablokujProces(id);//blokowanie procesu gdy niue odczytal
-							}							//(pierwsza w kolejce) to wyswietli sie 
-						
-																		//"Wiadomosc zostala odebrana: tresc", jesli nie, 
-																		//poinformuje "Wiadomosc nie zostala odebrana (brak wiadomosci w skrzynce)"
-																		//!!! DODAM ZE sprawdza czy skrzynka istnieje, 
-																		//jesli nie to: "Wiadomosc nie zostala odebrana, skrzynka nie istnieje"
-																		//i tu nalezy proces zawiesic@@@@@@@@@@@@@@@@@@@@@@@@@
-							if(s.startsWith("XS")){ 										//komenda do wysylania komunikatu
-								pw.println("->KOMENDA XS ");
-								Wiadomosc wiadomosc = new Wiadomosc(id,s.substring(3)); 	//skrzynka procesu wysylajacego wiadomosc ma numer procesu,
-								IPC.wyslij(wiadomosc, id); 					//	wiadomosc ma ten sam numer
-							}						//metoda odpowiedzialna za utworzenie skrzynki 
-						
-							if(s.startsWith("XD")){  				//zakonczenie procesu <do testow>
-								pw.println("->KOMENDA XD ");
-								pw.println("*proces "+id+"konczy dzialanie* ");
-								IPC.usunSkrzynke(id);  				//metoda usuwajaca skrzynke <powinna zawrzec sie w miejscu gdzy proces sie konczy!!!!!
-								ListaProcesow.usunProces(id); //usuniecie procesu, proteza
-								dzialanie=false;
-							}
-							if(s.startsWith("SAB")){
-								IPC.wszystkieSkrzynki();  			//metoda wyswietla kolejno utworzone skrzynki dzialajacych procesow wraz z zawartosciami
-							}
-									
-							//DO WYSWIETLANIA KONKRETNEJ SKRZYNKI
-							if(s.startsWith("SB")){
-								int numer = Integer.parseInt(str.substring(3,4)); 	//pobranie numeru procesu
-								IPC.informacje(numer);  							//metoda wyswietla dana skrzynke z zawartoscia,
-							}							
-						}
-					//<jesli nie istnieje> i wyslanie do niej komunikatu		
-							
-							
-					if(str.startsWith("SAB")){
-						IPC.wszystkieSkrzynki();  			//metoda wyswietla kolejno utworzone skrzynki dzialajacych procesow wraz z zawartosciami
+			str = br.readLine(); 			//wpisanie komendy
+			
+				if(str.startsWith("XC")){ 		//komenda do tworzenia procesu
+					pw.println("->KOMENDA XC");  	//tak o zebym wiedzial ze zadzialalo to akurat xd zbedne
+					id++;
+					j++;
+					//ListaProcesow.wyswietlWszystkieProcesy(); testowne odpowiedzi
+					//ListaProcesow.wyswietlPCBProcesu(id);
+					//ListaProcesow.dodajProces(id);
+					//ListaProcesow.wyswietlPCBProcesu(id);
+					//ListaProcesow.wyswietlWszystkieProcesy();
+					nazwaPliku = str.substring(3); 							//podzielenie wiersza z komenda aby wydobyc nazwe pliku wejsciowego
+					sciezka = "c:\\JavaSO\\"+nazwaPliku+".txt"; 	//utworzenie sciezki do pliku aby go otworzyc
+					cz[j] = new Czytnik(id,sciezka);
 					}
-							
-					//DO WYSWIETLANIA KONKRETNEJ SKRZYNKI
-					if(str.startsWith("SB")){
-						int numer = Integer.parseInt(str.substring(3,4)); 	//pobranie numeru procesu
-						IPC.informacje(numer);  							//metoda wyswietla dana skrzynke z zawartoscia,
-					}															//jesli skrzynka/proces nie istnieje, wyswietli komunikat:
-				}																//"Skrzynka procesu *ID* nie istnieje, wyswietlenie zawartosci 
-																					//nie powiodlo sie"
-			 					//zamkniecie pliku
-					}while(dzialanie!=false);
-					fr.close();
+				if(str.startsWith("cd")){
+					if(!cz[i].Przetworz()) //ZMIANA PROCESU
+						i++; 
+					if(i>j)
+						i=0;
 					}
+				
+				if(str.startsWith("SAB")){
+					try {
+						IPC.wszystkieSkrzynki(); //metoda wyswietla kolejno utworzone skrzynki dzialajacych procesow wraz z zawartosciami
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}  			
+				}
+							
+				//DO WYSWIETLANIA KONKRETNEJ SKRZYNKI
+				if(str.startsWith("SB")){
+					int numer = Integer.parseInt(str.substring(3,4)); 	//pobranie numeru procesu
+					try {
+						IPC.informacje(numer);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}  							//metoda wyswietla dana skrzynke z zawartoscia,
+				}								//jesli skrzynka/proces nie istnieje, wyswietli komunikat:
+												//"Skrzynka procesu *ID* nie istnieje, wyswietlenie zawartosci 
+												//nie powiodlo sie"		 						
 			//DO WYJSCIA Z SYSTEMU
 			if(str.startsWith("EXIT"))
 				wyjscie = true;
 					
 		}while(wyjscie != true);
-		
-		
-		
 		pw.println();
 		pw.println();
 		pw.println("@@@@  @   @  @  @@@@@");
