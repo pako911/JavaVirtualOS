e1cc48b33e71db2a29bf9c42eda782d2f6d58159
pedeef <przemyslaw.lapicz@gmail.com>
2017-01-11 13:38:06 +0100

    Update Wiadomosc.java

--- src/komunikacjaMiedzyprocesowa/Wiadomosc.java
+++ src/komunikacjaMiedzyprocesowa/Wiadomosc.java
@@ -2,11 +2,13 @@ package komunikacjaMiedzyprocesowa;
 
 public class Wiadomosc {
 	private int IDnadawcy;
+	private int IDodbiorcy;
 	private String trescWiadomosci;
 	private int rozmiarWiadomosci;
 	
-	public Wiadomosc(int IDnadawcy,String trescWiadomosci){ //wiadomosc zawiera od kogo wiadomosc (IDnadawcy), tresc (trescWiadomosci) 							
+	public Wiadomosc(int IDnadawcy, int IDodbiorcy,String trescWiadomosci){ //wiadomosc zawiera od kogo wiadomosc (IDnadawcy), tresc (trescWiadomosci) 							
 		this.IDnadawcy = IDnadawcy;							//i rozmiarWiadomosci - nie uzyty pozniej, chyba ze ograniczaamy dlugosc wiadomosci	
+		this.IDodbiorcy = IDodbiorcy;
 		this.trescWiadomosci = trescWiadomosci;
 		this.rozmiarWiadomosci = trescWiadomosci.length();
 	}	//boolean
@@ -18,13 +20,18 @@ public class Wiadomosc {
 		return true;
 	}
 	
-	public int pobierzIDprocesu(){
+	public int pobierzIDnadawcy(){
 		return IDnadawcy;
 	}
+	
+	public int pobierzIDodbiorcy(){
+		return IDodbiorcy;
+	}
+	
 	public int pobierzRozmiarWiadomosci(){
 		return rozmiarWiadomosci;
 	}
 	public void wyswietlInfo(){	//tj wyswietla wiadomosc konkretna
-		System.out.println(this.pobierzWiadomosc()+"("+this.rozmiarWiadomosci+")");
+		System.out.println(this.pobierzIDnadawcy()+": "+this.pobierzWiadomosc());
 	}
 }
