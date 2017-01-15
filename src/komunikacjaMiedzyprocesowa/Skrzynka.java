a5aecea50f15b3700978d9e8f9933a2ba70d549a
pedeef <przemyslaw.lapicz@gmail.com>
2017-01-15 21:30:32 +0100

    Update Skrzynka.java

--- src/komunikacjaMiedzyprocesowa/Skrzynka.java
+++ src/komunikacjaMiedzyprocesowa/Skrzynka.java
@@ -1,37 +1,79 @@
 package komunikacjaMiedzyprocesowa;
 
-public class Wiadomosc {
-	private int IDnadawcy;
-	//private int IDodbiorcy;
-	private String trescWiadomosci;
-	private int rozmiarWiadomosci;
-	//								int IDodbiorcy
-	public Wiadomosc(int IDnadawcy, String trescWiadomosci){ //wiadomosc zawiera od kogo wiadomosc (IDnadawcy), tresc (trescWiadomosci) 							
-		this.IDnadawcy = IDnadawcy;		
-		this.trescWiadomosci = trescWiadomosci;
-		this.rozmiarWiadomosci = trescWiadomosci.length();
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+
+public class Skrzynka {
+	//			List								LinkedList
+	private final Queue<Wiadomosc> messages = new ArrayDeque<>(); //kolejka wiadomosci w skrzynce procesu
+	private int wlascicielSkrzynki;								//numer procesu
+	private int numerSkrzynki;
 	
-	}	//boolean
-	public String pobierzWiadomosc(){  //boolean - po dluzszej analizie doszedlem do wniosku ze latwiej pozniej w programie glownym operowac na true/false
-		return trescWiadomosci;
+	public Skrzynka(int wlascicielSkrzynki, int numerSkrzynki){ 					//konstruktor
+		this.wlascicielSkrzynki = wlascicielSkrzynki;
+		this.numerSkrzynki=numerSkrzynki;
+	}
+	public void dodajWiadomosc(Wiadomosc wiadomosc){ 			//dodawanie do skrzynki w kolejce
+		this.messages.add(wiadomosc);
+	}
+	public int pobierzWlascicielaSkrzynki(){ 					//odbieranie wiadomosci i usuwanie jej ze skrzynki
+		return wlascicielSkrzynki;
 	}
 	
-	public boolean czyOdebrana(){  //boolean - po dluzszej analizie doszedlem do wniosku ze latwiej pozniej w programie glownym operowac na true/false
-		return true;
+	public int pobierzNumerSkrzynki(){ 					//odbieranie wiadomosci i usuwanie jej ze skrzynki
+		return numerSkrzynki;
 	}
 	
-	public int pobierzIDnadawcy(){
-		return IDnadawcy;
+	//								int nrOdbiorcy
+	public boolean pobierzWiadomosc(){ //boolean aby w IPC wyznaczac true i false
+		//int i=0;
+		if(messages.isEmpty()){  								//sprawdza czy nie pusta
+			System.out.println("Wiadomosc nie zostala odebrana (brak wiadomosci w skrzynce)");
+			return false;  										//powrot
+			}
+		else{
+		//	for(Wiadomosc x : messages){
+		//		if(x.pobierzIDodbiorcy()==nrOdbiorcy){
+				Wiadomosc wiadomosc = this.messages.poll();//this.messages.get(i); 		//pobiera pierwszy element z listy i usuwa go
+			//	this.messages.remove(i);
+				System.out.println("Wiadomosc zostala odebrana: "+wiadomosc.pobierzWiadomosc());
+				return true;				//return wiadomosc//niewazne
+		//		}
+				//i++;
+		//	}
+			//System.out.println("W skrzynce nie ma wiadomosci dla tego procesu");
+			//return false;
+		}
 	}
 	
-	/*public int pobierzIDodbiorcy(){
-		return IDodbiorcy;
+	public void wyswietlStanSkrzynki(){
+		System.out.println("Skrzynka nr "+pobierzNumerSkrzynki()+", wlasciciel PID: "+pobierzWlascicielaSkrzynki());
+		System.out.println(messages.size()+" wiad. ");
+		if(!messages.isEmpty()){
+			System.out.println("-----------------");
+			for(Wiadomosc x : messages)
+	            x.wyswietlInfo();
+		System.out.println("-----------------");
+		System.out.println("");
+		}
+	}
+	
+	/*public boolean sprawdzWiadomosci(int idodbiorcy){
+		for(Wiadomosc x : messages){
+			if(x.pobierzIDodbiorcy()==idodbiorcy)
+				return true;
+		}
+		return false;
 	}*/
 	
-	public int pobierzRozmiarWiadomosci(){
-		return rozmiarWiadomosci;
+	//w Czytnik dodane bylo przy usuwaniu procesu i zarazem skrzynki!
+	public void usunWiadomosci(){
+		for(Wiadomosc x : messages){
+			messages.remove(x);
+		}
 	}
-	public void wyswietlInfo(){	//tj wyswietla wiadomosc konkretna
-		System.out.println(this.pobierzWiadomosc());
-	} 	//					this.pobierzIDodbiorcy()+
 }
